1b063d7499ff171d26873aeed3729d2197f90d72
gabrielsan179 <gabrielsan179@gmail.com>
2023-03-09 09:18:19 -0300

    :label: add types

---
+++ src/dtos/commentDTO.ts
@@ -0,0 +1,29 @@
+import { CommentModel } from '../types';
+
+export interface GetCommentsInput {
+    token: string | undefined
+}
+
+export type GetCommentsOutput = CommentModel[]
+
+export interface CreateCommentInput {
+    token: string | undefined,
+    content: unknown
+}
+
+export interface EditCommentInput {
+    idToEdit: string,
+    token: string | undefined,
+    content: unknown
+}
+
+export interface DeleteCommentInput {
+    idToDelete: string,
+    token: string | undefined
+}
+
+export interface LikeOrDislikeCommentInput {
+    idToLikeOrDislike: string,
+    token: string | undefined,
+    like: unknown
+}
