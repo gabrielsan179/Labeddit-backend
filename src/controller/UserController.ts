30e0d6cdf56a56379c2b2a50a824b34d366f3567
gabrielsan179 <gabrielsan179@gmail.com>
2023-03-17 22:30:11 -0300

    deploy

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { UserBusiness } from "../business/UserBusiness"
-import { GetUsersInput, LoginInput, SignupInput } from "../dtos/UserDTO"
+import { GetUsersInput, LoginInput, SignupInput } from "../dtos/userDTO"
 import { BaseError } from "../errors/BaseError"
 
 export class UserController {
